c56afc73b1b6c2242c868ddbedb9520e86e460ac
charliegordon <cgordon5@illinois.edu>
2019-12-01 20:12:34 -0600

    UI Setup

---
+++ app/src/main/java/com/example/cs125finalproject/FillInGame.java
@@ -0,0 +1,14 @@
+package com.example.cs125finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FillInGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fill_in_game);
+    }
+}
---
+++ app/src/main/java/com/example/cs125finalproject/NewGame.java
@@ -0,0 +1,14 @@
+package com.example.cs125finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_game);
+    }
+}
